38130774dcc114730d7c32f03a330760544078aa
saodasinei <39985466+saodasinei@users.noreply.github.com>
2019-01-22 01:03:54 +0100

    Add files via upload

---
+++ public/js/home.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+         // $("#user-icon").css('display','block');
+         // $("#logout-icon").css('display','block');
+         // $("#logout-icon").css('display','none');
+	});
