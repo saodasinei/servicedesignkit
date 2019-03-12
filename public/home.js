6d37c2dc79aa9a9b2d9410c51597571327a9df51
saodasinei <39985466+saodasinei@users.noreply.github.com>
2019-03-12 18:37:21 +0100

    Delete home.js

--- public/home.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-
-         // $("#user-icon").css('display','block');
-         // $("#logout-icon").css('display','block');
-         // $("#logout-icon").css('display','none');
-	});
